d1039c822101c3fb4c1ce285fd6f8efda975690e
Ziyad <ziyad@Ziyads-MacBook-Pro.local>
2019-12-16 00:21:32 +0000

    Added setup script

--- Framework/Project.php
+++ Framework/Project.php
@@ -1,16 +1,19 @@
 <?php 
 
 class ProjectStructure {
+
     public function __construct($dirname){
-        // checks if a directory exists
+        // Checks if a directory exists
         
         if(!is_dir(__DIR__.'/' .$dirname)) {
-        // creates the project structure
+              
+            // Creates the project structure
+
               mkdir($dirname);
               mkdir($dirname.'/'.'App');
               mkdir($dirname.'/'.'App/php');
               mkdir($dirname.'/'.'App/views');
-              mkdir($dirname.'/'.'App/controllers');
+              mkdir($dirname.'/'.'App/backend');
               touch($dirname.'/'.'router.dk');
               mkdir($dirname.'/'.'Database');
               touch($dirname.'/'.'config.toml');
@@ -18,7 +21,7 @@ class ProjectStructure {
             } else {
               echo "ERROR => directory exists".PHP_EOL;
             }
-        }
+    }
 }
 
 ?>
--- file.php
+++ file.php
@@ -1,9 +1,4 @@
 <?php
-// $test $of $dakar $lang; 
-print("Hello"); 
-$name = 10; 
-echo($name); 
-function love($name): array{
-    return $name ; 
-}
+
+
 ?>
