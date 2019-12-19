c3888ad5e8dd2a5c6c4b9f08b6ecd3721846cc9f
Ziyad <ziyad@Ziyads-MacBook-Pro.local>
2019-12-19 00:20:04 +0000

    fixed installation with composer

---
+++ Framework/ProjectStructure.php
@@ -0,0 +1,28 @@
+<?php 
+
+class ProjectStructure {
+
+    public function __construct($dirname){
+        // Checks if a directory exists
+        
+        if(!is_dir(__DIR__.'/' .$dirname)) {
+              
+            // Creates the project structure
+
+              mkdir($dirname);
+              mkdir($dirname.'/'.'App');
+              mkdir($dirname.'/'.'App/.php');
+              mkdir($dirname.'/'.'App/views');
+              mkdir($dirname.'/'.'App/backend');
+              touch($dirname.'/'.'router.dk');
+              mkdir($dirname.'/'.'Database');
+              touch($dirname.'/'.'config.toml');
+              shell_exec("cp -R ~/.composer/vendor/dakar-framework/dakar-framework/Framework .");
+              echo("Project $dirname created!");
+            } else {
+              echo "ERROR => directory exists".PHP_EOL;
+            }
+    }
+}
+
+?>
