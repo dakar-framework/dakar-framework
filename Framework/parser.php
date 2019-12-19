e3eb135865a6588e6387af899853d5e148111918
Ziyad <ziyad@Ziyads-MacBook-Pro.local>
2019-12-19 18:49:07 +0000

    few fixes arround translation

--- Framework/parser.php
+++ Framework/parser.php
@@ -4,16 +4,15 @@ require_once("processor.php");
 class Parser {
     
     public function __construct(){ }
-    public static function parse($file_name) {
+    public static function parse($file_name,$production_file_location) {
         $parsed_file = Processor::process($file_name);
         array_unshift($parsed_file,"<?php" . PHP_EOL);
         array_push($parsed_file,"?>");
         $parsed_file = implode("",$parsed_file);
         $file_name = explode(".",$file_name)[0];
-        $php_file = fopen($file_name . ".php", 'w');
+        $php_file = fopen("$production_file_location/$file_name.php", 'w');
         fwrite($php_file,$parsed_file);
         fclose($php_file);
-
     }
 }   
 
