e76339f07719c16a59912c31a6128e2524fb15bb
Ziyad <ziyad@Ziyads-MacBook-Pro.local>
2019-12-23 05:22:27 +0000

    added foreach loop and control structures

--- Framework/DKTemplateEngine/DKTemplateEngine.php
+++ Framework/DKTemplateEngine/DKTemplateEngine.php
@@ -5,7 +5,7 @@
         # unless
         # #json => json_encode
         # comments 
-        private static  $KEYWORDS = [
+        private static $KEYWORDS = [
             "for",
             "while",
             "if",
@@ -15,7 +15,7 @@
             "break",
             "continue",
         ];
-        
+
         // Will translate tdk files to php file .
         public static function compile($tdk_file,$output_destination)  {
             
@@ -27,9 +27,10 @@
             
             // Walk the array
             foreach($t_file as &$line) {
-
-               self::replace_echo_DKTemplate($line);
-               self::replace_directive_DKtemplate($line);
+                self::replace_include_DKTemplate($line);
+                self::replace_echo_DKTemplate($line);
+                self::replace_directive_DKTemplate($line);
+                self::replace_directive_without_paren_DKTemplate($line);
                 array_push($rendered_template,$line);
             }
 
@@ -42,51 +43,72 @@
         }
        
         // replace echo statements #()
-        public static function replace_echo_DKTemplate(&$line) {
+        private static function replace_echo_DKTemplate(&$line) {
             $regex = '/\#\(.*\)/';
             preg_match($regex,$line,$expression_found);
-           
             if($expression_found){
-                echo $expression_found[0] . "\n";
-                if($expression_found[0][1] == "(") {
-                        $expr = $expression_found[0];
-                        $expression_found[0] = str_replace("#("," ",$expression_found[0]);
-                        $expression_found[0] = str_replace(")"," ",$expression_found[0]);
-                        $expression_found[0] = trim($expression_found[0]);
-
-                    if(strpos($expression_found[0],",")) {
-                        $vars = explode(",",$expression_found[0]);
-                        foreach($vars as $v) {
-                        $expression_found[0]= str_replace($v,"$".$v,$expression_found[0]);
+                $to_evaluate = $expression_found[0];
+                $to_evaluate = ltrim($to_evaluate,"#(");
+                $to_evaluate = substr($to_evaluate, 0, -1);
+            
+            // detect functions
+            $function_regex = "/\w+\(.*\)/";
+            preg_match($function_regex,$to_evaluate,$evalute_function);
+            // if a function found 
+            if($evalute_function){
+                if($evalute_function[0]){
+                $evalute_function[0] = substr($evalute_function[0], 0, -1);
+                $eval_array = explode("(",$evalute_function[0]);
+                       // params
+                        if($eval_array[1]){
+                            // multiple params
+                            if(strpos($eval_array[1],",")){
+                                $params_string = [];
+                                $func_params = explode(",",$eval_array[1]);
+                                foreach($func_params as $param){
+                                    array_push($params_string,"$$param");
+                                }
+                                $params_string = implode(",",$params_string);
+                                $to_evaluate = "$eval_array[0]($params_string)";
+                                $line = str_replace($expression_found[0],"<?= $to_evaluate ?>" ,$line);
+                            } else {
+                                $to_evaluate = "$eval_array[0]($$eval_array[1])";
+                                $line = str_replace($expression_found[0],"<?= $to_evaluate) ?>",$line);
+                            }
                         }
-                    } else {
-                        $expression_found[0] = str_replace($expression_found[0],"$".$expression_found[0],$expression_found[0]); 
+                        else {
+                            // if there is no params
+                            $to_evaluate = $evalute_function[0] . ")";
+                        }
                     }
-                    $line = str_replace($expr,"<?= $expression_found[0] ?>",$line);
-           
-                    
-                echo("(-------------------------)\n");
+               
+                } 
+                else {
+                $echo_params = [];
+                // multiple params to echo
+                if(strpos($to_evaluate,",")){
+                    $to_evaluate = explode(",",$to_evaluate);
+                    foreach($to_evaluate as $v){
+                        array_push($echo_params,"$$v");
+                    }
+                    $echo_params = implode(",",$echo_params);
 
-                // switch($funcOrOperator[0]){
-                //     case "for":
-                //         echo "will be evaluated : $to_evaluate\n";
-                //     break ; 
-                // }            
-                // if(in_array($funcOrOperator,self::$KEYWORDS)){
-                    
-                //     echo " >> " . $funcOrOperator . "\n";
-                //     echo " >> " .  $expression_found[0]. "\n";
-                // }
-            }
-        
-        } 
-     
-        
-    }
-    public static function replace_directive_DKtemplate(&$line) {
+                $to_evaluate = "$echo_params";
+              }else {
+                $to_evaluate = str_replace($to_evaluate,"$".$to_evaluate,$to_evaluate); 
+              }
+        }
+            
+            $line = str_replace($expression_found[0],"<?= $to_evaluate ?>",$line);
+        }
+    } 
+    
+    //
+    private static function replace_directive_DKTemplate(&$line) {
         $regex = '/\#\w+\(.*\)/';
         preg_match($regex,$line,$expression_found);
         if($expression_found){
+            $rendered_expr = "";
             $expr = $expression_found[0];
             $expr = explode("(",$expr);
             $directive = $expr[0];
@@ -107,24 +129,73 @@
                             $range = explode("..",$to_evaluate[1]);
                             $rendered_evaluation = "($$to_evaluate[0]=$range[0];$$to_evaluate[0]<=$range[1];$$to_evaluate[0]++){";
                             $rendered_expr = "<?php $directive $rendered_evaluation ?>";
-                            $line = str_replace($expression_found,$rendered_expr,$line);
                         } else {
                             echo "bad range supplied!" . "\n";
                         }
 
                     } else {
-                        echo "bad range supplied" , "\n";
+                        $rendered_evaluation ="($$to_evaluate[1] as $$to_evaluate[0])";
+                        $rendered_expr = "<?php $directive"."each" . "$rendered_evaluation ?>";
                     }
                 }
             }
-            //$rendered_expr = $directive . " " . $to_evaluate; 
-            //$line = str_replace($expression_found,$rendered_expr,$line);
+            if($directive == "if" || $directive == "elseif" || $directive == "while"){
+                $to_evaluate_array = explode(" ",$to_evaluate);
+                foreach($to_evaluate_array as $v){
+                    preg_match("/[a-zA-Z]/",$v,$var_found);
+                    if($var_found){
+                       $to_evaluate_array = str_replace($v,"$$var_found[0]",$to_evaluate_array);                    
+                    }
+                }
+                $to_evaluate_array = implode("",$to_evaluate_array);
+                $rendered_expr = "<?php $directive($to_evaluate_array) { ?>";
+            }
+           
+            $line = str_replace($expression_found,$rendered_expr,$line);
         }
     }
+
+    //
+    private static function replace_directive_without_paren_DKTemplate(&$line){
+        $regex = '/\#\w+\s*(?!.+)/';
+        preg_match($regex,$line,$expression_found);
+        if($expression_found) {
+            $directive = trim($expression_found[0]);
+            $directive = ltrim($directive,"#");
+            $returned_expr = "";
+            switch($directive){
+                case "end": 
+                    $returned_expr = "}";
+                break ; 
+                case "continue":
+                    $returned_expr = $directive.";";
+                break ; 
+                case "break":
+                    $returned_expr = $directive.";";
+                break ;
+                case "else":
+                    $returned_expr = "} else {";
+                break ; 
+                default:
+            }
+            $line = str_replace($expression_found[0],"<?php $returned_expr ?>",$line); 
+        }
+    }
+
+    //
+    private static function replace_include_DKTemplate(&$line){
+        $regex = "/\#\{.*\}/";
+        preg_match($regex,$line,$expression_found);
+        if($expression_found){
+            $expr = ltrim($expression_found[0],"#{");
+            $expr = rtrim($expr,"}");
+            $line = str_replace($expression_found[0],"<?php include(".$expr."); ?>",$line);
+        }
+
+    }
 }
-    DKTemplateEngine::compile("file.tdk",".");
-
-
-
-
+    
+        DKTemplateEngine::compile("file.tdk",".");
+        
+        
 ?>
